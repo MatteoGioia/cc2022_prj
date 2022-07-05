33ee281c2efcb24d01f8a8b01ae4d7cf423553be
MatteGioia <matteo.gioia1999@gmail.com>
2022-07-05 12:36:35 +0200

    Fix commit script

--- config.sh
+++ config.sh
@@ -5,9 +5,10 @@ yum update -y
 yum install git -y
 
 #Clone cycle gan official repo
+cd /home/ec2-user
 git clone https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix code
 chown -R ec2-user code
-cd pytorch-CycleGAN-and-pix2pix
+cd code
 
 #Install requirements
 python3 -m pip install -r requirements.txt
