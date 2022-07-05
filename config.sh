e009a54ccf7bca86eeb1bd6b8d31721b2440370c
MatteGioia <matteo.gioia1999@gmail.com>
2022-07-05 11:07:45 +0200

    Config fix ownership

--- config.sh
+++ config.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+#Install git
+yum update -y
+yum install git -y
+
 #Clone cycle gan official repo
-git clone https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix
+git clone https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix code
+chown -R ec2-user code
 cd pytorch-CycleGAN-and-pix2pix
 
 #Install requirements
