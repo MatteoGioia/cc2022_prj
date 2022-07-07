190221a9ba6d55d06ee7d9948628269d4f9bc139
MatteGioia <matteo.gioia1999@gmail.com>
2022-07-07 17:40:01 +0200

    Added upload button

--- demo.py
+++ demo.py
@@ -15,11 +15,21 @@ def download_model(model):
     condition = "[[ ! -f checkpoints/{}_pretrained/latest_net_G.pth ]] &&".format(model)
     os.system(condition + download_md_command)
 
-def test(model):
+def test(model, force=False):
+
+    nr_test_A = len(os.listdir("datasets/{}/testA".format(model)))
+    nr_test_B = len(os.listdir("datasets/{}/testB".format(model)))
+
     model_pretrained = model + "_pretrained"
-    test_command = "python3 test.py --dataroot datasets/{}/testA --name {} --model test --no_dropout --gpu_ids -1".format(model, model_pretrained)
+    test_command = "python3 test.py --dataroot datasets/{}/{} --name {} --model test --direction {} --num_test {} --no_dropout --gpu_ids -1"
     condition = "[[ ! \"$(ls -A results/{}_pretrained/test_latest/images)\" ]] &&".format(model)
-    os.system(condition + test_command)
+    
+    if not force:
+        os.system(condition + test_command.format(model, "testaA", model_pretrained,"AtoB",nr_test_A) + test_command.format(model, "testaB", model_pretrained,"BtoA",nr_test_B))
+    else:
+         os.system(test_command.format(model, "testaA", model_pretrained,"AtoB",nr_test_A) + test_command.format(model, "testaB", model_pretrained,"BtoA",nr_test_B))
+
+
 
 def get_img_list(model):
     img_list = os.listdir("results/{}_pretrained/test_latest/images".format(model))
@@ -32,7 +42,7 @@ def get_img_list(model):
 models = ["apple2orange", "summer2winter_yosemite", "horse2zebra", "monet2photo", "iphone2dslr_flower"]
 
 st.title("CycleGAN online demo")
-model = st.selectbox('Choose the model and dataset to use', models, index=3)
+model = st.selectbox('Choose the model and dataset to use', models, index=2)
 
 #Download dataset, model
 download_dataset(model)
@@ -59,6 +69,15 @@ with col2:
     st.image(img, caption="real")
 
 #Upload your own sample
+file = st.file_uploader(label="Upload your own sample to conver from a A to B (warning: reloads website!", type=["png", "jpg"],
+        accept_multiple_files=False)
+
+test_dir = "./datasets/{}/testB".format(model)
+if file is not None:
+    with open(os.path.join(test_dir, "0.jpg"), "wb") as f:
+        f.write(file.getbuffer())
+
+    test(model, force=True)
 
 
 st.text("Credit: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix")
