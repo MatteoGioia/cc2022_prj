1d7752188e217012deee03d2b65ab6aed72993d2
MatteoGioia <matteo.gioia1999@gmail.com>
2022-07-10 11:54:41 +0200

    Upload file test

--- demo.py
+++ demo.py
@@ -45,12 +45,11 @@ def test(model, force=False):
     else:
         os.system(test_command.format(model, "testA", model_pretrained,"AtoB",nr_test_A) + " && " + test_command.format(model, "testB", rev_model_pretrained,"BtoA",nr_test_B))
 
-def test_single(model):
+def test_single(model, usr_dir, direction):
 
     model_pretrained = model + "_pretrained"
-    #TODO: per model folders
-    test_command = "python3 test.py --dataroot user_imgs --name {} --model test --direction {} --no_dropout --gpu_ids -1"  
-    os.system(test_command.format())
+    test_command = "python3 test.py --dataroot {} --name {} --model test --direction {} --no_dropout --gpu_ids -1"  
+    os.system(test_command.format(usr_dir, model_pretrained, direction))
 
 def get_img_list(model):
     img_list = os.listdir("results/{}_pretrained/test_latest/images".format(model))
@@ -95,24 +94,34 @@ with col2:
     img = plt.imread('./results/{}_pretrained/test_latest/images/{}_real.png'.format(used_model, sample))
     st.image(img, caption="real")
 
-"""
 #Upload your own sample
 file = st.file_uploader(label="Upload your own sample", type=["png", "jpg"],
         accept_multiple_files=False)
-mod = st.selectbox("Select model", ["A2B", "B2A"])
+mod = st.selectbox("Select mode", ["A2B", "B2A"], key="sb2")
 
 if mod == "A2B":
     chosen_model = model
 else:
     chosen_model = get_reverse_model(model)
 
-user_imgs = "usr_imgs"
 if file is not None:
-    with open(os.path.join(user_imgs, "0.jpg"), "wb") as f:
+    #Create directory for user files
+    usr_dir = "{}/{}".format(chosen_model, file.name)
+    os.system("mdkir {}".format(usr_dir))
+    with open(os.path.join(usr_dir, "0.jpg"), "wb") as f:
         f.write(file.getbuffer())
 
-    test_single(chosen_model)
-"""
+    test_single(chosen_model, usr_dir, direction = mod)
 
+"""
+usr_col1, usr_col2 = st.columns(2)
+with usr_col1:
+    img = plt.imread('./results/{}_pretrained/test_latest/images/{}_fake.png'.format(used_model, sample))
+    st.image(img, caption="fake")
+
+with usr_col2:
+    img = plt.imread('./results/{}_pretrained/test_latest/images/{}_real.png'.format(used_model, sample))
+    st.image(img, caption="real")
+""" 
 
 st.text("Credit: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix")
